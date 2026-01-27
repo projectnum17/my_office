b27a612370677e242571f70785b61474fea8e080
projectnum17 <egor.chornyy@gmail.com>
2026-01-27 19:25:26 +0200

    add fix to coursee

--- assets/scripts/mods/sectionServices.js
+++ assets/scripts/mods/sectionServices.js
@@ -42,5 +42,49 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     };
+
+    const subserviceTab = () => {
+        const tabsParent = document.querySelector('.js-services-parent');
+        if (!tabsParent) return;
+
+        const tabs = tabsParent.querySelectorAll('.js-services-tab');
+        const tabsContent = document.querySelectorAll('.js-service-box');
+
+        if (!tabs.length || !tabsContent.length) return;
+
+        const hideTabContent = () => {
+            tabsContent.forEach((item) => {
+                item.classList.add('tab-hide');
+                item.classList.remove('tab-show', 'tab-fade');
+            });
+
+            tabs.forEach((item) => {
+                item.classList.remove('is-active');
+            });
+        };
+
+        const showTabContent = (i = 0) => {
+            tabsContent[i].classList.add('tab-show', 'tab-fade');
+            tabsContent[i].classList.remove('tab-hide');
+            tabs[i].classList.add('is-active');
+        };
+
+        hideTabContent();
+        showTabContent();
+
+        tabsParent.addEventListener('click', (event) => {
+            const tab = event.target.closest('.js-services-tab');
+            if (!tab) return;
+
+            tabs.forEach((item, i) => {
+                if (tab === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        });
+    };
+
+    subserviceTab();
     servicesTabHandler();
 });
